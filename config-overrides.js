71ce89bc61d290fe2f373f14b75fc322463460d9
MinhLoc282 <minhloc2802@gmail.com>
2024-02-13 10:04:39 +0700

    fix

--- config-overrides.js
+++ config-overrides.js
@@ -1,46 +1,38 @@
+const path = require('path');
 const webpack = require('webpack');
-const { useBabelRc, override } = require('customize-cra');
 
-module.exports = override(
-  useBabelRc(),
-  (config) => {
-    // Add babel-loader rule specifically for ConstantsUtil.js in scaffold-utils
-    config.module.rules.push({
-      test: /\.js$/, // Adjust the test pattern if necessary
-      include: /node_modules[\\/]@web3modal[\\/]scaffold-utils/, // Target the scaffold-utils module
-      use: {
-        loader: require.resolve('babel-loader'), // Use babel-loader directly
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: [
-            ['@babel/plugin-syntax-import-attributes', { deprecatedAssertSyntax: true }]
-          ]
-        }
-      }
-    });
+module.exports = function override(config) {
+  const babelLoaderRule = config.module.rules.find(rule => rule.oneOf);
 
-    // Add webpack plugin for resolving certain modules in Node.js environment
-    const fallback = config.resolve.fallback || {};
-    Object.assign(fallback, {
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "assert": require.resolve("assert"),
-      "http": require.resolve("stream-http"),
-      "https": require.resolve("https-browserify"),
-      "os": require.resolve("os-browserify"),
-      "url": require.resolve("url"),
-      'process/browser': require.resolve('process/browser')
-    });
-    config.resolve.fallback = fallback;
+  // Add the '@babel/plugin-syntax-import-attributes' to the Babel configuration
+  babelLoaderRule.oneOf.forEach(loader => {
+    if (loader.loader && loader.loader.includes('babel-loader')) {
+      // Ensure loader.options.plugins is initialized as an array
+      loader.options.plugins = loader.options.plugins || [];
+      // Push the plugin to the array
+      loader.options.plugins.push('@babel/plugin-syntax-import-attributes');
+    }
+  });
 
-    // Add webpack plugin for providing global variables
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        process: 'process/browser',
-        Buffer: ['buffer', 'Buffer']
-      })
-    );
+  // config.resolve.alias['@web3modal/wagmi'] = path.resolve(__dirname, 'src/custom-web3modal-wagmi');
 
-    return config;
-  }
-);
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify"),
+    "url": require.resolve("url"),
+    'process/browser': require.resolve('process/browser')
+  });
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]);
+  return config;
+}
